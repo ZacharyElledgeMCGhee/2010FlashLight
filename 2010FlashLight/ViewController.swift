1b3d7180c6d1a8b4f2d6355f39b9882ccc463afb
ZacharyElledgeMCGhee <zte18a@acu.edu>
2020-10-13 18:50:04 -0700

    made changes

---
+++ 2010FlashLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2010FlashLight
+//
+//  Created by Zach on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
